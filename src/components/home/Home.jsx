9afc9300ebbc3b4a10d5ce762f46a37ac99792f4
Jaideep Singh <jaisidhu2004@gmail.com>
2024-01-21 20:56:14 +0530

    21/1/24

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -19,46 +19,34 @@ export default function Home() {
 		}, 2000);
 	}, []);
 
-	
-
 	return (
-		<div
-			id='home'
-			className='bg'>
-			<div className="background">
-				<div className="circle1"></div>
-				<div className="circle2"></div>
-				<div className="circle3"></div>
-				<div className="circle4"></div>
-				<div className="circle5"></div>
-				<div className="circle6"></div>
-				
+		<div id='home' className='bg'>
+			<div className='background'>
+				<div className='circle1'></div>
+				<div className='circle2'></div>
+				<div className='circle3'></div>
+				<div className='circle4'></div>
+				<div className='circle5'></div>
+				<div className='circle6'></div>
 			</div>
 			<div className='hero'>
-			
-				<div className='name'  data-aos="fade-right" data-aos-duration="400">
+				<div className='name' data-aos='fade-right' data-aos-duration='400'>
 					{loading && (
 						<div className='typeanimation'>
 							<TypeAnimation
 								cursor={false}
-								sequence={[
-									"JAIDEEP SINGH",
-									
-								]}
+								sequence={["JAIDEEP SINGH"]}
 								speed={10}
 							/>
 						</div>
 					)}
-					<div id="name1">
-					
+					<div id='name1'>
 						<div>Student || Front-End Developer</div>
-						
 					</div>
 				</div>
 
-				<div className='heroimage' data-aos="zoom-in" data-aos-duration="400">
-					<Monitor/>
-					
+				<div className='heroimage' data-aos='zoom-in' data-aos-duration='400'>
+					<Monitor />
 				</div>
 			</div>
 		</div>
